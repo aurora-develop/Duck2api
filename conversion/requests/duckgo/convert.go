1a7a446fbc558d210afd66e6345d9df98055157b
xiaozhou26 <xiaoxiaofeihh@foxmail.com>
2024-05-02 14:45:56 +0800

    Update duckgo convert.go to use claude-3-haiku-20240307 model

--- conversion/requests/duckgo/convert.go
+++ conversion/requests/duckgo/convert.go
@@ -11,7 +11,7 @@ func ConvertAPIRequest(api_request officialtypes.APIRequest) duckgotypes.ApiRequ
 	duckgo_request := duckgotypes.NewApiRequest("gpt-3.5-turbo-0125")
 	// 检查并更新模型为 claude- 开头的情况
 	if strings.HasPrefix(strings.ToLower(api_request.Model), "claude") {
-		duckgo_request.Model = "claude-instant-1.2"
+		duckgo_request.Model = "claude-3-haiku-20240307"
 	}
 	content := buildContent(&api_request)
 	duckgo_request.AddMessage("user", content)
