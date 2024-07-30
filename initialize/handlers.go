33b9b57d49da306c9ac8347831867084505efded
John Lin <john.lin@ringcentral.com>
2024-07-30 17:34:58 +0800

    perf: Updated supported model to gpt-4o-mini

--- initialize/handlers.go
+++ initialize/handlers.go
@@ -95,7 +95,7 @@ func (h *Handler) engines(c *gin.Context) {
 
 	// Supported models
 	modelIDs := []string{
-		"gpt-3.5-turbo-0125",
+		"gpt-4o-mini",
 		"claude-3-haiku-20240307",
 		"meta-llama/Llama-3-70b-chat-hf",
 		"mistralai/Mixtral-8x7B-Instruct-v0.1",
